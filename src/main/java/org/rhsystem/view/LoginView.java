2b29911c24da47d4db8c83ff95a8fa1b08748487
Vinicim <vinicius_anacleto@estudante.sesisenai.org.br>
2025-09-25 18:43:08 -0300

    #11 feat: Desenvolvimento da classe LoginView.

--- src/main/java/org/rhsystem/view/LoginView.java
+++ src/main/java/org/rhsystem/view/LoginView.java
@@ -1,4 +1,15 @@
 package org.rhsystem.view;
 
+import java.util.Scanner;
+
+import org.rhsystem.model.Login;
+
 public class LoginView {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static int interfaceLogin(){
+        System.out.println("\n|| ------- Login ------- ||");
+        System.out.println("")
+    }
 }
