612f754ecfed61535c6dce020e1f3f94002aa458
DANIEL MULLER <daniel_muller150@estudante.sesisenai.org.br>
2025-09-25 19:32:08 -0300

    #13 feat: Implementa metodo de alterar senha

--- src/main/java/org/rhsystem/controller/FuncionarioController.java
+++ src/main/java/org/rhsystem/controller/FuncionarioController.java
@@ -1,4 +1,39 @@
 package org.rhsystem.controller;
 
+import org.rhsystem.model.Usuario;
+import org.rhsystem.service.FuncionarioService;
+import org.rhsystem.view.FuncionarioView;
+import org.rhsystem.view.MessagesHelper;
+
 public class FuncionarioController {
+
+    public static void funcionarioController(Usuario usuario) {
+        do {
+            int opcao = FuncionarioView.menuFuncionario();
+            switch (opcao) {
+
+                case 1: {
+                    FuncionarioView.visualizarInformacoes(usuario);
+                    break;
+                }
+
+                case 2: {
+
+                }
+
+                case 3: {
+                    FuncionarioService.alterarSenha(usuario);
+                }
+
+                case 0: {
+                    return;
+                }
+
+                default: {
+                    MessagesHelper.error("Opção inválida!");
+                }
+            }
+
+        }while (true);
+    }
 }
