4dcd9d39226d501e85154fa46d7bd7211ad5b60c
jonathan_uber <jonathanuber121207@gmail.com>
2025-10-01 21:48:02 -0300

    feat: cria métodos do DAO de relatórios

--- src/main/java/org/rhsystem/dao/RelatorioDAO.java
+++ src/main/java/org/rhsystem/dao/RelatorioDAO.java
@@ -1,8 +1,38 @@
 package org.rhsystem.dao;
 
+import org.rhsystem.model.Cargo;
+import org.rhsystem.model.Departamento;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RelatorioDAO {
 
     public static int totalUsuariosExistentes(){
         return 0;
+    }
+
+    public static int totalCargosExistentes(){
+        return 0;
+    }
+
+    public static int totalDepartamentosExistentes(){
+        return 0;
+    }
+
+    public static Map<Departamento, Integer> totalFuncPorDepartamento(){
+        return Map<Departamento, Integer> funcPorDepartamento = new HashMap<>();
+    }
+
+    public static Map<Cargo, Integer> totalFuncPorCargo(){
+        return Map<Cargo, Integer> funcPorCargo = new HashMap<>();
+    }
+
+    public static Map<Departamento, Double> mediaSalarialPorDepartamento(){
+        return Map<Departamento, Integer> funcPorDepartamento = new HashMap<>();
+    }
+
+    public static Map<Cargo, Double> mediaSalarialPorCargo(){
+        return Map<Cargo, Integer> funcPorCargo = new HashMap<>();
     }
 }
